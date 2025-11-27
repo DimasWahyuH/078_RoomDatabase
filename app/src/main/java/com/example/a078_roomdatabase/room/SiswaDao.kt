3906f4359bfe042a8a4b7275a5184c8c1b530358
dimas <dimaswh19@gmail.com>
2025-11-27 13:27:44 +0700

    Menambahkan interface SiswaDao untuk Room database

---
+++ app/src/main/java/com/example/a078_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,8 @@
+package com.example.a078_roomdatabase.room
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface SiswaDao
