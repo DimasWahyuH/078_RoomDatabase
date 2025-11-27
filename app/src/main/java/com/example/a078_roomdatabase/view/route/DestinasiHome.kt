d4d99753d82b241afff0f93d0aba9ec20fb861cd
dimas <dimaswh19@gmail.com>
2025-11-27 15:07:25 +0700

    Mengimplementasikan properti route dan titleRes pada DestinasiHome

--- app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiHome.kt
@@ -2,4 +2,7 @@ package com.example.a078_roomdatabase.view.route
 
 import com.example.a078_roomdatabase.R
 
-object DestinasiHome: DestinasiNavigasi 
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
