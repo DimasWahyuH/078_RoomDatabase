7cf89d1133cb4c9fe5cf7c1c81eb9b8226bac1cb
dimas <dimaswh19@gmail.com>
2025-11-27 13:38:18 +0700

    Mengimplementasikan ContainerDataApp dan menambahkan kelas AplikasiSiswa

--- app/src/main/java/com/example/a078_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a078_roomdatabase/repositori/ContainerApp.kt
@@ -8,4 +8,20 @@ interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
-class ContainerDataApp
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+
+class AplikasiSiswa : Application() {
+     lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
