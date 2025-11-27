a0ceee76be61a05cef08c3485c8a0892dcd557f8
dimas <dimaswh19@gmail.com>
2025-11-27 13:36:34 +0700

    Menambahkan interface ContainerApp

---
+++ app/src/main/java/com/example/a078_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,7 @@
+package com.example.a078_roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.a078_roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp
