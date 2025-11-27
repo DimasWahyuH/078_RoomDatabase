ae18066cafd380524cff29e833db8f9b1a2cb34a
dimas <dimaswh19@gmail.com>
2025-11-27 15:08:17 +0700

    Menambahkan properti route dan titleRes pada DestinasiEntry

--- app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiEntry.kt
@@ -2,4 +2,7 @@ package com.example.a078_roomdatabase.view.route
 
 import com.example.a078_roomdatabase.R
 
-object DestinasiEntry : DestinasiNavigasi
+object DestinasiEntry : DestinasiNavigasi{
+    override val route =  "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
