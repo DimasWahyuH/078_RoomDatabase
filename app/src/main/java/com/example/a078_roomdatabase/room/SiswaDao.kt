23c12c3bc6be2366eeb9636b8a468e1140778eec
dimas <dimaswh19@gmail.com>
2025-11-27 13:28:04 +0700

    Menambahkan method getAllSiswa dan insert pada interface SiswaDao

--- app/src/main/java/com/example/a078_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a078_roomdatabase/room/SiswaDao.kt
@@ -5,4 +5,11 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-interface SiswaDao
+interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+}
