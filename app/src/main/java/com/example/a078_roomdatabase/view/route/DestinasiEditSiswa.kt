29ed50cef69feae507154d3542354589619b170c
dimas <dimaswh19@gmail.com>
2025-12-04 18:40:20 +0700

    Menambahkan objek DestinasiEditSiswa sebagai tujuan navigasi

--- app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -2,4 +2,9 @@ package com.example.a078_roomdatabase.view.route
 
 import com.example.a078_roomdatabase.R
 
-object DestinasiEditSiswa: DestinasiNavigasi
+object DestinasiEditSiswa: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
