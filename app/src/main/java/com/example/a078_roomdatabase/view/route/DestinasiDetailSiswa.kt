fee099347e7724e53ec5d68d341e203ce226853e
dimas <dimaswh19@gmail.com>
2025-12-04 18:38:58 +0700

    Menambahkan implementasi DestinasiDetailSiswa untuk navigasi

--- app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/a078_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -2,4 +2,10 @@ package com.example.a078_roomdatabase.view.route
 
 import com.example.a078_roomdatabase.R
 
-object DestinasiDetailSiswa : DestinasiNavigasi
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
