9c3c14e332c38183c07868eb5358c0d1378aeac6
dimas <dimaswh19@gmail.com>
2025-12-04 18:32:56 +0700

    Menambahkan navigasi ke DetailSiswaScreen dan EditSiswaScreen pada PetaNavigasi

--- app/src/main/java/com/example/a078_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a078_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a078_roomdatabase.view.DetailSiswaScreen
+import com.example.a078_roomdatabase.view.EditSiswaScreen
 import com.example.a078_roomdatabase.view.EntrySiswaScreen
 import com.example.a078_roomdatabase.view.HomeScreen
+import com.example.a078_roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.a078_roomdatabase.view.route.DestinasiEditSiswa
 import com.example.a078_roomdatabase.view.route.DestinasiEntry
 import com.example.a078_roomdatabase.view.route.DestinasiHome
 
@@ -28,11 +34,9 @@ fun HostNavigasi(
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                //edit 1 : tambahkan parameter navigateToItemsUpdate
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/$it")
+                }
             )
         }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
-        }
-
-    }
-}
